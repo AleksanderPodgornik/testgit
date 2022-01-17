87f750c6c2e4f1bcd90700cfcfcd0ec294c8e264
AleksanderPodgornik <97885976+AleksanderPodgornik@users.noreply.github.com>
2022-01-17 19:37:36 +0100

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -4,10 +4,7 @@ public class Hello
    public static void Main()
    {
 	System.Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-	System.Console.WriteLine("Hello, Local!");
-=======
 	System.Console.WriteLine("Hello, GitHub!");
->>>>>>> ed3f4aa2ca51d00c56fedc36817207c6c31d06e9
+	System.Console.WriteLine("Hello, Local!");
    }
 }
